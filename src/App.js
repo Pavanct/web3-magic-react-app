c4679b671fc52b0bfc04c783cd79421cb82ae17a
Pavan Chitradurga Thammanna <mail@pavanct.com>
2022-03-21 01:54:36 +0100

    add user context to persist state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material"
+import { CircularProgress, Paper, Typography } from "@mui/material"
 import { useEffect, useState } from "react"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import "./App.css"
@@ -6,42 +6,56 @@ import PrivateRoute from "./components/PrivateRoute"
 import Dashboard from "./Pages/Dashboard"
 import Login from "./Pages/Login"
 import { checkUser } from "./services/magic"
+import { UserContext } from "./context/UserContext"
 
 function App() {
-  const [userEmail, setUserEmail] = useState("")
   const [isLoggedIn, setIsLoggedIn] = useState(false)
   const [address, setAddress] = useState("")
+  const [isLoading, setLoading] = useState(true)
 
   useEffect(() => {
     checkUser()
-      .then((a) => {
-        setIsLoggedIn(a.isLoggedIn)
-        setUserEmail(a.email)
-        setAddress(a.publicAddress)
+      .then(({ isLoggedIn, publicAddress }) => {
+        setIsLoggedIn(isLoggedIn)
+        setAddress(publicAddress)
+        setLoading(false)
       })
       .catch((e) => console.error(e))
   }, [isLoggedIn])
 
+  if (isLoading) {
+    return (
+      <div className="progress-bar">
+        <CircularProgress color="info" />
+      </div>
+    )
+  }
+
   return (
     <div className="App">
-      <Typography variant="h4" gutterBottom component="div">
-        React Magic Link with Web3
-      </Typography>
-      {/* {isLoggedIn ? <Dashboard /> : <Login />} */}
-      <Router>
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={
-              <PrivateRoute isLoggedIn={isLoggedIn}>
-                <Dashboard address={address} />
-              </PrivateRoute>
-            }
-          ></Route>
-          <Route path="/login" exact element={<Login />}></Route>
-        </Routes>
-      </Router>
+      <Paper
+        elevation={15}
+        style={{ padding: "30px", minHeight: "48vh", minWidth: "30vw" }}
+      >
+        <Typography variant="h4" gutterBottom component="div">
+          Magic Wallet
+        </Typography>
+        <UserContext.Provider value={isLoggedIn}>
+          <Router>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <PrivateRoute>
+                    <Dashboard address={address} />
+                  </PrivateRoute>
+                }
+              ></Route>
+              <Route path="/login" element={<Login />}></Route>
+            </Routes>
+          </Router>
+        </UserContext.Provider>
+      </Paper>
     </div>
   )
 }
